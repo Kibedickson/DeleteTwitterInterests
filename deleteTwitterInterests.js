f64bf9c3bce3603148065eaa71f7f210f33350cf
Dickson <kibedickson3@gmail.com>
2023-05-02 13:39:29 +0300

    wip

--- deleteTwitterInterests.js
+++ deleteTwitterInterests.js
@@ -1,13 +1,4 @@
-/*
- * 
- * https://twitter.com/settings/your_twitter_data/twitter_interests 
- * 
- * Go to the inspector and run this code and it should untick all the ticked boxes
- * in the list. You can change the interval to less than 650ms. You need a delay
- * or the twitter API thinks you're trying to overload it.
- * 
- */
-var checkboxes = [];
+let checkboxes = [];
 // we only really want to find those that are checked/ticked
 // so instead of looping over all of them (with an interval)
 // only loop over the checked ones, and put them into
@@ -17,10 +8,10 @@ document.querySelectorAll('input[type="checkbox"]').forEach((c) => {
     checkboxes[checkboxes.length] = c; 
   }
 });
-var i = 0;
+let i = 0;
 
-var loopId = setInterval(() => {
-  var e = checkboxes[i++];
+let loopId = setInterval(() => {
+  let e = checkboxes[i++];
   if(i % 10 == 0) { console.log("Running... (" + i + ")")}
   if(e !== undefined) {
     if(e.checked) {
@@ -30,4 +21,4 @@ var loopId = setInterval(() => {
   } else {
     clearInterval(loopId);
   }
-}, 3000);
+}, 7000);
